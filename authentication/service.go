448a5337c01254458a638e6e67d9c55eb036a47f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-07 23:20:12 +0000

    adding a Opts struct to configure the de authentication service

--- authentication/service.go
+++ authentication/service.go
@@ -1,17 +1,41 @@
 package authentication
 
-import "time"
+import (
+	"time"
+)
 
-type Service struct {
-	sessionCookieName     string
-	sessionCookieDuration time.Duration
-	sessionCookieDomain   string
-}
+type (
+	Service struct {
+		sessionCookieName     string
+		sessionCookieDuration time.Duration
+		sessionCookieDomain   string
+	}
 
-func New() *Service {
-	return &Service{
+	Opts struct {
+		sessionCookieName     string
+		sessionCookieDuration time.Duration
+		sessionCookieDomain   string
+	}
+)
+
+var (
+	defaults = Opts{
 		sessionCookieName:     "_fnncr_session",
 		sessionCookieDuration: 24 * time.Hour, // One Day
 		sessionCookieDomain:   "localhost",
+	}
+)
+
+func New() *Service {
+	var (
+		opts = defaults
+	)
+
+	// TODO: Implement a mechanism to configure the authentication service
+
+	return &Service{
+		sessionCookieName:     opts.sessionCookieName,
+		sessionCookieDuration: opts.sessionCookieDuration,
+		sessionCookieDomain:   opts.sessionCookieDomain,
 	}
 }

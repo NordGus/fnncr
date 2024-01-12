4457090a8975c3d16f509410669c295f6d60cb3e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-12 17:02:18 +0000

    changing PostgreSQLOpts fields

--- repository/usersrepo/rpstry_postgresql.go
+++ repository/usersrepo/rpstry_postgresql.go
@@ -3,11 +3,10 @@ package usersrepo
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/NordGus/fnncr/authentication"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -18,8 +17,20 @@ type (
 	}
 
 	PostgreSQLOpts struct {
-		Options pgxpool.Config
-		Ctx     context.Context
+		Ctx context.Context
+
+		Host     string
+		Port     uint16
+		Database string
+		User     string
+		Password string
+
+		MaxConns              int32
+		MinConns              int32
+		MaxConnLifetime       time.Duration
+		MaxConnLifetimeJitter time.Duration
+		MaxConnIdleTime       time.Duration
+		HealthCheckPeriod     time.Duration
 	}
 
 	PostgreSQLConfigFunc func(opts *PostgreSQLOpts)
@@ -29,23 +40,17 @@ var (
 	ErrInvalidPostgreSQLOpts = errors.New("usersrepo: invalid postgresql opts")
 
 	postgresqlDefaults = PostgreSQLOpts{
-		Options: pgxpool.Config{
-			ConnConfig: &pgx.ConnConfig{
-				Config: pgconn.Config{
-					Host:     "127.0.0.1",
-					Port:     5432,
-					Database: "fnncr_dev",
-					User:     "fnncr",
-					Password: "local_dev",
-				},
-			},
-			MaxConns:              3,
-			MinConns:              1,
-			MaxConnLifetime:       15 * time.Second,
-			MaxConnLifetimeJitter: 2 * time.Second,
-			MaxConnIdleTime:       30 * time.Second,
-			HealthCheckPeriod:     20 * time.Second,
-		},
+		Host:                  "127.0.0.1",
+		Port:                  5432,
+		Database:              "fnncr_dev",
+		User:                  "fnncr",
+		Password:              "local_dev",
+		MaxConns:              3,
+		MinConns:              1,
+		MaxConnLifetime:       15 * time.Second,
+		MaxConnLifetimeJitter: 2 * time.Second,
+		MaxConnIdleTime:       30 * time.Second,
+		HealthCheckPeriod:     20 * time.Second,
 	}
 )
 
@@ -62,7 +67,12 @@ func NewPostgreSQLRepository(configs ...PostgreSQLConfigFunc) (*PostgreSQLReposi
 		return nil, ErrInvalidPostgreSQLOpts
 	}
 
-	pool, err := pgxpool.NewWithConfig(opts.Ctx, &opts.Options)
+	config, err := pgxpool.ParseConfig(opts.connString())
+	if err != nil {
+		return nil, errors.Join(ErrInvalidPostgreSQLOpts, err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(opts.Ctx, config)
 	if err != nil {
 		return nil, errors.Join(ErrInvalidPostgreSQLOpts, err)
 	}
@@ -87,4 +97,21 @@ func (repo *PostgreSQLRepository) Close() error {
 	repo.client.Close()
 
 	return nil
+}
+
+func (opts PostgreSQLOpts) connString() string {
+	return fmt.Sprintf(
+		"user=%s password=%s host=%s port=%d dbname=%s pool_max_conns=%d pool_min_conns=%d pool_max_conn_lifetime=%s pool_max_conn_idle_time=%s pool_health_check_period=%s pool_max_conn_lifetime_jitter=%s",
+		opts.User,
+		opts.Password,
+		opts.Host,
+		opts.Port,
+		opts.Database,
+		opts.MaxConns,
+		opts.MinConns,
+		opts.MaxConnLifetime,
+		opts.MaxConnIdleTime,
+		opts.HealthCheckPeriod,
+		opts.MaxConnLifetimeJitter,
+	)
 }

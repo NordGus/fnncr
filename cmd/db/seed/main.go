bfc4efe1f387f4873d017e2efcc4603fae25daf7
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-23 21:37:32 +0000

    adding a simple command to add a user to the database

---
+++ cmd/db/seed/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	pgserv "github.com/NordGus/fnncr/database/postgresql"
+	"github.com/NordGus/fnncr/internal/adapters/secondary/postgres"
+	"github.com/NordGus/fnncr/internal/core/services/users"
+)
+
+func main() {
+	var (
+		pg = pgserv.New(
+			os.Getenv("PG_DB_USERNAME"),
+			os.Getenv("PG_DB_PASSWORD"),
+			os.Getenv("PG_DB_HOST"),
+			os.Getenv("PG_DB_PORT"),
+			os.Getenv("PG_DB_DATABASE"),
+			func(db *sql.DB) { db.SetMaxOpenConns(10) },
+			func(db *sql.DB) { db.SetMaxIdleConns(5) },
+			func(db *sql.DB) { db.SetConnMaxIdleTime(15 * time.Second) },
+		)
+
+		usersrepo = postgres.NewUsersRepository(pg.DB())
+
+		usersserv = users.NewService(usersrepo)
+	)
+	defer pg.Close()
+
+	resp, err := usersserv.Create(context.Background(), users.CreateReq{
+		Username:             "jrico",
+		Password:             "localdev",
+		PasswordConfirmation: "localdev",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(resp)
+}

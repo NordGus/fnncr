07da1de548327243bd904f90dcb5c558849a5644
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-24 03:30:46 +0000

    removing unnecessary Password and moving it a new method to build a PasswordDigest from a password and adding to it the password validation

--- internal/core/domain/user/val_passwordigest.go
+++ internal/core/domain/user/val_passwordigest.go
@@ -1,19 +1,53 @@
 package user
 
-import "errors"
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type PasswordDigest string
 
+const (
+	passwordMinLen = 8
+	passwordMaxLen = 64
+)
+
 var (
-	ErrPasswordDigestEmpty = errors.New("password digest is empty")
+	ErrPasswordEmpty    = errors.New("user: password is empty")
+	ErrPasswordTooShort = errors.New("user: password is too short")
+	ErrPasswordTooLong  = errors.New("user: password is too long")
 )
 
 func NewPasswordDigest(digest string) (PasswordDigest, error) {
 	if digest == "" {
-		return "", ErrPasswordDigestEmpty
+		return "", ErrPasswordEmpty
 	}
 
 	return PasswordDigest(digest), nil
+}
+
+func NewPasswordDigestFromPassword(pw string) (PasswordDigest, error) {
+	length := len(pw)
+
+	if length == 0 {
+		return "", ErrPasswordEmpty
+	}
+
+	if length < passwordMinLen {
+		return "", ErrPasswordTooShort
+	}
+
+	if length > passwordMaxLen {
+		return "", ErrPasswordTooLong
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return PasswordDigest(hash), nil
 }
 
 func (pwd PasswordDigest) String() string {

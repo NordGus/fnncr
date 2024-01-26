ae0defb953c4597d3b1eaa645342d7fa9a6b8813
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-26 23:52:50 +0000

    initial accounts applet design design

--- internal/adapters/primary/web/app/accounts/handler.go
+++ internal/adapters/primary/web/app/accounts/handler.go
@@ -8,6 +8,15 @@ const (
 	// Routes
 
 	AppletRoute = "/accounts"
+
+	// TODO(#0): All accounts can have a parent, this is used to create categories each one.
+	// TODO(#1): Personal accounts convey bank accounts, savings accounts and credit lines/cards. Credit lines/cards
+	// can be added as expenses for easier cataloging of payments in the application and following of personal expenses.
+	PersonalAccountsRoute = "/accounts/personal"
+	// TODO(#2): Debt accounts are basically all loans or any other debt you can have.
+	DebtAccountsRoute = "/accounts/debts"
+	// TODO(#3): External are basically transaction categories but in this case are easier administer.
+	ExternalAccountsRoute = "/accounts/external"
 )
 
 type Handler interface {
--- internal/adapters/primary/web/app/accounts/handler_section.go
+++ internal/adapters/primary/web/app/accounts/handler_section.go
@@ -1,10 +1,19 @@
 package accounts
 
 import (
-	view "github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/application"
+	view "github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/accounts"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *handler) AppletHandlerFunc(c echo.Context) error {
-	return view.NotImplemented(AppletRoute).Render(c.Request().Context(), c.Response())
+	p := view.Page{
+		Title: "Accounts",
+		Sections: []view.Section{
+			{Title: "Personal", Id: "personal", FetchURL: PersonalAccountsRoute},
+			{Title: "Debts", Id: "debts", FetchURL: DebtAccountsRoute},
+			{Title: "External", Id: "external", FetchURL: ExternalAccountsRoute},
+		},
+	}
+
+	return view.Applet(p).Render(c.Request().Context(), c.Response())
 }

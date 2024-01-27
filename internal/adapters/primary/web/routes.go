115e82299cc1dfe8b526dd251ed11bfc41e8869c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-27 03:55:37 +0000

    implementing an shared applicationLayoutLoaderMiddleware to inject the application's ApplicationLayoutData in applets

--- internal/adapters/primary/web/routes.go
+++ internal/adapters/primary/web/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/authentication"
 	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/budget"
 	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/models"
+	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/shared"
 	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/summary"
 	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/transactions"
 	views "github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/application"
@@ -31,34 +32,69 @@ func (a *App) setRoutes() {
 	a.echo.POST(authentication.SignInRoute, auth.SignInHandlerFunc)
 	a.echo.GET(authentication.SignOutRoute, auth.SignOutHandlerFunc, auth.AuthorizeMiddleware)
 
-	a.echo.GET(accounts.AppletRoute, accnts.AppletHandlerFunc, auth.AuthorizeMiddleware)
+	a.echo.GET(
+		accounts.AppletRoute,
+		accnts.AppletHandlerFunc,
+		auth.AuthorizeMiddleware,
+		applicationLayoutLoaderMiddleware,
+	)
 
-	a.echo.GET(transactions.AppletRoute, trnsctns.AppletHandlerFunc, auth.AuthorizeMiddleware)
+	a.echo.GET(
+		transactions.AppletRoute,
+		trnsctns.AppletHandlerFunc,
+		auth.AuthorizeMiddleware,
+		applicationLayoutLoaderMiddleware,
+	)
 
-	a.echo.GET(budget.AppletRoute, bdgt.AppletHandlerFunc, auth.AuthorizeMiddleware)
+	a.echo.GET(
+		budget.AppletRoute,
+		bdgt.AppletHandlerFunc,
+		auth.AuthorizeMiddleware,
+		applicationLayoutLoaderMiddleware,
+	)
 
-	a.echo.GET(assets.AppletRoute, assts.AppletHandlerFunc, auth.AuthorizeMiddleware)
+	a.echo.GET(
+		assets.AppletRoute,
+		assts.AppletHandlerFunc,
+		auth.AuthorizeMiddleware,
+		applicationLayoutLoaderMiddleware,
+	)
 
-	a.echo.GET(summary.AppletRoute, smmry.AppletHandlerFunc, auth.AuthorizeMiddleware)
+	a.echo.GET(
+		summary.AppletRoute,
+		smmry.AppletHandlerFunc,
+		auth.AuthorizeMiddleware,
+		applicationLayoutLoaderMiddleware,
+	)
 
 	a.echo.GET("/", func(c echo.Context) error {
-		ald := layouts.ApplicationLayoutData{
-			Title: "fnncr",
-			UserOptionNave: components.NavItemWithDropdownData{
-				Name: c.Get(authentication.CurrentUserCtxKey).(models.User).Username,
-				Options: []components.NavItemData{
-					{Name: "Sign Out", Route: authentication.SignOutRoute},
-				},
-			},
-			NavItems: []components.NavItemData{
-				{Name: "accounts", Route: accounts.AppletRoute},
-				{Name: "transactions", Route: transactions.AppletRoute},
-				{Name: "budget", Route: budget.AppletRoute},
-				{Name: "assets", Route: assets.AppletRoute},
-				{Name: "summary", Route: summary.AppletRoute},
-			},
-		}
+		ald := c.Get(shared.ALDContextKey).(layouts.ApplicationLayoutData)
 
 		return views.Root(ald).Render(c.Request().Context(), c.Response())
-	}, auth.AuthorizeMiddleware)
+	}, auth.AuthorizeMiddleware, applicationLayoutLoaderMiddleware)
+}
+
+func applicationLayoutLoaderMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Set(shared.ALDContextKey,
+			layouts.ApplicationLayoutData{
+				Title: "fnncr",
+				UserOptionNave: components.NavItemWithDropdownData{
+					Name: c.Get(authentication.CurrentUserCtxKey).(models.User).Username,
+					Options: []components.NavItemData{
+						{Name: "Sign Out", Route: authentication.SignOutRoute},
+					},
+				},
+				NavItems: []components.NavItemData{
+					{Name: "accounts", Route: accounts.AppletRoute},
+					{Name: "transactions", Route: transactions.AppletRoute},
+					{Name: "budget", Route: budget.AppletRoute},
+					{Name: "assets", Route: assets.AppletRoute},
+					{Name: "summary", Route: summary.AppletRoute},
+				},
+			},
+		)
+
+		return next(c)
+	}
 }

71ea5b8586d26bff983d67ed87af09df2e34a91a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-01-27 04:04:14 +0000

    refactoring notimplemented view to work as a normal view with layout

--- internal/adapters/primary/web/app/transactions/handler_section.go
+++ internal/adapters/primary/web/app/transactions/handler_section.go
@@ -1,10 +1,14 @@
 package transactions
 
 import (
+	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/shared"
 	view "github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/application"
+	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/layouts"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *handler) AppletHandlerFunc(c echo.Context) error {
-	return view.NotImplemented(AppletRoute).Render(c.Request().Context(), c.Response())
+	ald := c.Get(shared.ALDContextKey).(layouts.ApplicationLayoutData)
+
+	return view.NotImplemented(ald, AppletRoute).Render(c.Request().Context(), c.Response())
 }

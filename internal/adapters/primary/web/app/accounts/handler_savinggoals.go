1205e98bc37e456d70e4a741ed365bddefe9e05c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-02-06 11:05:32 +0000

    implementing saving goals mock

--- internal/adapters/primary/web/app/accounts/handler_savinggoals.go
+++ internal/adapters/primary/web/app/accounts/handler_savinggoals.go
@@ -1,11 +1,15 @@
 package accounts
 
 import (
-	"net/http"
-
+	"github.com/NordGus/fnncr/internal/adapters/primary/web/app/models"
+	view "github.com/NordGus/fnncr/internal/adapters/primary/web/app/views/accounts"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *handler) SavingGoalsHandlerFunc(c echo.Context) error {
-	return c.NoContent(http.StatusNoContent)
+	goals := []models.SavingsGoal{
+		{GoalName: "Goal 1", Goal: 1000000, Achieved: 177300},
+	}
+
+	return view.SavingGoals(goals).Render(c.Request().Context(), c.Response())
 }

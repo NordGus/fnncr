cc36baa7e5671209f832a58807f6b7a720728142
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-02-10 14:26:47 +0000

    solving styling for accounts preview

--- internal/adapters/primary/web/client/src/application.js
+++ internal/adapters/primary/web/client/src/application.js
@@ -1,1 +1,6 @@
 import "htmx.org"
+import AccountPreview from "./components/AccountPreview";
+
+console.log("hello there");
+
+customElements.define("account-preview", AccountPreview);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./client/src/**/*.js", "./**/views/**/*.templ"],
+  content: ["./**/client/src/**/*.js", "./**/views/**/*.templ"],
   theme: {
     darkMode: "class",
     fontFamily: {
@@ -10,7 +10,7 @@ module.exports = {
       gridTemplateColumns: {
         'layout': '10vw 1fr',
         'accounts': '1fr 35vw',
-        'account-preview': 'min-content 1fr'
+        'account-preview': 'min-content 1fr min-content'
       },
       gridTemplateRows: {
         'accounts': "min-content 1fr",
@@ -18,7 +18,29 @@ module.exports = {
       },
       borderWidth: {
         'button': '1px'
-      }
+      },
+      animation: {
+        "marquee": "scroll-left 5s linear -1s infinite alternate"
+      },
+      keyframes: {
+        "scroll-left": {
+          "0%": {
+            "-moz-transform": "translateX(0%)",
+            "-webkit-transform": "translateX(0%)",
+            "transform": "translateX(0%)"
+          },
+          "20%": {
+            "-moz-transform": "translateX(0%)",
+            "-webkit-transform": "translateX(0%)",
+            "transform": "translateX(0%)"
+          },
+          "100%": {
+            "-moz-transform": "translateX(-75%)",
+            "-webkit-transform": "translateX(-75%)",
+            "transform": "translateX(-75%)"
+          }
+        },
+      },
     },
   },
   plugins: [],

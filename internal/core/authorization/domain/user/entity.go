e6fae33de446e865711a908417b291aa268c956f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-02-25 21:29:12 +0100

    setting up user entity

---
+++ internal/core/authorization/domain/user/entity.go
@@ -0,0 +1,37 @@
+package user
+
+import (
+	"financo/internal/core/authorization/domain/user/creationtime"
+	"financo/internal/core/authorization/domain/user/passworddigest"
+	"financo/internal/core/authorization/domain/user/sessionversion"
+	"financo/internal/core/authorization/domain/user/updatetime"
+	"financo/internal/core/authorization/domain/user/username"
+	"github.com/google/uuid"
+)
+
+type Entity struct {
+	ID             uuid.UUID
+	Username       username.Value
+	PasswordDigest passworddigest.Value
+	SessionVersion sessionversion.Value
+	CreateAt       creationtime.Value
+	UpdatedAt      updatetime.Value
+}
+
+func New(
+	id uuid.UUID,
+	un username.Value,
+	pw passworddigest.Value,
+	sv sessionversion.Value,
+	ct creationtime.Value,
+	ut updatetime.Value,
+) Entity {
+	return Entity{
+		ID:             id,
+		Username:       un,
+		PasswordDigest: pw,
+		SessionVersion: sv,
+		CreateAt:       ct,
+		UpdatedAt:      ut,
+	}
+}

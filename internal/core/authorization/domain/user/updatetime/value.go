27c8a23fcfc37827bf94dde86b2d5763208fe315
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-02-26 22:14:27 +0100

    implementing new value object standard for updatetime

--- internal/core/authorization/domain/user/updatetime/value.go
+++ internal/core/authorization/domain/user/updatetime/value.go
@@ -1,13 +1,30 @@
 package updatetime
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrEmpty = errors.New("must be present")
+)
 
 type Value struct {
 	updatedAt time.Time
 }
 
 func New(updatedAt time.Time) (Value, error) {
+	var errs error
+
+	if updatedAt.Equal(time.Time{}) {
+		errs = errors.Join(errs, ErrEmpty)
+	}
+
 	return Value{
-		updatedAt: updatedAt,
-	}, nil
+		updatedAt: updatedAt.UTC(),
+	}, errs
+}
+
+func (v *Value) Time() time.Time {
+	return v.updatedAt
 }

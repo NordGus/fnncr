872677e8f78251f983e0b9f298f42caf3b58c0ec
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-02-26 22:14:37 +0100

    implementing new value object standard for creationtime

--- internal/core/authorization/domain/user/creationtime/value.go
+++ internal/core/authorization/domain/user/creationtime/value.go
@@ -1,7 +1,30 @@
 package creationtime
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrEmpty = errors.New("must be present")
+)
 
 type Value struct {
 	createdAt time.Time
+}
+
+func New(createdAt time.Time) (Value, error) {
+	var errs error
+
+	if createdAt.Equal(time.Time{}) {
+		errs = errors.Join(errs, ErrEmpty)
+	}
+
+	return Value{
+		createdAt: createdAt.UTC(),
+	}, errs
+}
+
+func (v *Value) Time() time.Time {
+	return v.createdAt
 }

a86fc78106bdabff004757a24d136fc0b6bf22c6
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-03-05 22:14:25 +0100

    adding signout command

---
+++ internal/core/authorization/commands/signout/command.go
@@ -0,0 +1,72 @@
+package signout
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"financo/internal/core/authorization/domain/sessionversion"
+	"financo/internal/core/authorization/domain/timestamp"
+	"financo/internal/core/authorization/domain/user"
+)
+
+type (
+	Command interface {
+		Execute(req Request) Response
+	}
+
+	UserRepository interface {
+		Save(ctx context.Context, entity user.Entity) error
+	}
+
+	Request struct {
+		ctx  context.Context
+		user user.Entity
+	}
+
+	Response struct {
+		err error
+	}
+
+	command struct {
+		userRepository UserRepository
+	}
+)
+
+func New(userRepository UserRepository) Command {
+	return &command{
+		userRepository: userRepository,
+	}
+}
+
+func NewRequest(ctx context.Context, user user.Entity) Request {
+	return Request{
+		ctx:  ctx,
+		user: user,
+	}
+}
+
+func (c *command) Execute(req Request) Response {
+	var (
+		res Response
+	)
+
+	ver, err := sessionversion.New(req.user.SessionVersion().Uint32() + 1)
+	if res.err = errors.Join(err, res.err); res.err != nil {
+		return res
+	}
+
+	ut, err := timestamp.New(time.Now())
+	if res.err = errors.Join(err, res.err); res.err != nil {
+		return res
+	}
+
+	err = c.userRepository.Save(
+		req.ctx,
+		user.New(req.user.ID(), req.user.Username(), req.user.PasswordDigest(), ver, req.user.CreatedAt(), ut),
+	)
+
+	res.err = errors.Join(err, res.err)
+
+	return res
+}

ee873ef6ffa4efd0d0bd702403e56e850c54e02e
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-03-09 12:01:57 +0100

    refactoring how a new service is initialized

--- internal/core/authorization/service.go
+++ internal/core/authorization/service.go
@@ -2,21 +2,10 @@ package authorization
 
 import (
 	"context"
-	"time"
 
 	"financo/internal/core/authorization/commands/authenticate"
 	"financo/internal/core/authorization/commands/signin"
 	"financo/internal/core/authorization/commands/signout"
-	"financo/internal/core/authorization/domain/session"
-	"financo/internal/core/authorization/domain/sessionID"
-	"financo/internal/core/authorization/domain/user"
-	"financo/internal/core/authorization/domain/userID"
-	"financo/internal/core/authorization/domain/username"
-)
-
-const (
-	SessionMaxAge   = time.Hour
-	SessionStaleAge = 24 * time.Hour
 )
 
 type (
@@ -26,18 +15,6 @@ type (
 		AuthenticateUser(ctx context.Context, sessionID string) AuthenticateUserResponse
 	}
 
-	UserRepository interface {
-		GetByID(ctx context.Context, id userID.Value) (user.Entity, error)
-		GetByUsername(ctx context.Context, value username.Value) (user.Entity, error)
-		Save(ctx context.Context, entity user.Entity) error
-	}
-
-	SessionRepository interface {
-		Get(ctx context.Context, id sessionID.Value) (session.Entity, error)
-		Create(ctx context.Context, entity session.Entity) error
-		Delete(ctx context.Context, id sessionID.Value) error
-	}
-
 	service struct {
 		signInCommand       signin.Command
 		signOutCommand      signout.Command
@@ -45,11 +22,11 @@ type (
 	}
 )
 
-func New(userRepository UserRepository, sessionRepository SessionRepository) API {
+func newService(signInCmd signin.Command, signOutCmd signout.Command, authenticateCmd authenticate.Command) API {
 	return &service{
-		signInCommand:       signin.New(userRepository, sessionRepository),
-		signOutCommand:      signout.New(userRepository),
-		authenticateCommand: authenticate.New(userRepository, sessionRepository, SessionMaxAge, SessionStaleAge),
+		signInCommand:       signInCmd,
+		signOutCommand:      signOutCmd,
+		authenticateCommand: authenticateCmd,
 	}
 }
 

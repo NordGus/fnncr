02cf3c641c1b648282ab77c814b6ae22f5a8c33b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-03-16 21:26:31 +0000

    restructuring db package

--- internal/database/postgresql/migrations/20240123165223_create_users_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY NOT NULL,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    password_digest VARCHAR NOT NULL,
-    session_version INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMP
-);
